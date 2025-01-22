fa5ed4f9c3e33204712739aaa583bcd1c11caa96
Ryan Forsyth <ryan.forsyth@digital.justice.gov.uk>
2025-01-22 14:40:25 +0000

    initial playwright setup

--- steps/auth/login.ts
+++ steps/auth/login.ts
@@ -1,12 +1,6 @@
 import { expect, type Page } from '@playwright/test'
 
 export const findAndReferLogin = async (page: Page) => {
-    // await page.goto(process.env.PREPARE_A_CASE_FOR_SENTENCE_URL)
-    // await expect(page).toHaveTitle(/HMPPS Digital Services - Sign in/)
-    // await page.fill('#username', process.env.DELIUS_USERNAME!)
-    // await page.fill('#password', process.env.DELIUS_PASSWORD!)
-
-
     await page.goto('http://localhost:3000');
     await page.getByLabel('Username').fill(process.env.HMPPS_AUTH_USERNAME as string)
     await page.getByLabel('Password').fill(process.env.HMPPS_AUTH_PASSWORD as string)
