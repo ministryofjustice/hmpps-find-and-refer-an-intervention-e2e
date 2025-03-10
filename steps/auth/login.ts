002b2f1356261d23ac291ab3aa9d7de974dc80ee
nia evans <nia.evans@digital.justice.gov.uk>
2025-03-10 11:38:40 +0000

    Update to use new users

--- steps/auth/login.ts
+++ steps/auth/login.ts
@@ -22,8 +22,8 @@ export const findAndReferLoginCommunity = async (page: Page) => {
 
 export const findAndReferUnauthorised = async (page: Page) => {
     await page.goto(process.env.FIND_AND_REFER_URL);
-    await page.getByLabel('Username').fill('robert.mercury')
-    await page.getByLabel('Password').fill('password123456')
+    await page.getByLabel('Username').fill(process.env.HMPPS_UNAUTHORISED_AUTH_USERNAME)
+    await page.getByLabel('Password').fill(process.env.HMPPS_UNAUTHORISED_AUTH_PASSWORD)
     await page.locator('#submit', { hasText: 'Sign in' }).click()
 
     await expect(page.getByText('Authorisation Error')).toBeVisible();
