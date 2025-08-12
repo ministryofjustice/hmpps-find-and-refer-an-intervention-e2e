ab370ab1a9c918e93782a28f2696f21418065068
kumar.thangavel <kumar.thangavel1@justice.gov.uk>
2025-08-12 16:55:22 +0100

    fixing the auth error page

--- steps/auth/login.ts
+++ steps/auth/login.ts
@@ -37,6 +37,6 @@ export const findAndReferUnauthorised = async (page: Page) => {
   await page.getByLabel('Password').fill(appConfig.HMPPS_UNAUTHORISED_AUTH_PASSWORD)
   await page.locator('#submit', { hasText: 'Sign in' }).click()
 
-  await expect(page.getByText('There is a problem')).toBeVisible()
-  await expect(page.getByText('Enter a valid username and password. You will be locked out if you enter the wrong details 10 times.')).toBeVisible()
+  await expect(page.getByText('Authorisation Error')).toBeVisible()
+  await expect(page.getByText('You are not authorised to use this application.')).toBeVisible()
 }
