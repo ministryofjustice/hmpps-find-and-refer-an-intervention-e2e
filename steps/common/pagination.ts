f44c338aa45774605e307a105b5d2dc32fdbb7de
kumar.thangavel <kumar.thangavel1@justice.gov.uk>
2026-03-19 12:08:04 +0000

    fixing the broken e2e

--- steps/common/pagination.ts
+++ steps/common/pagination.ts
@@ -1,9 +1,12 @@
 import { Page } from '@playwright/test'
 
 export const clickOnPaginationPage = async (page: Page, pageNumber: number) => {
-  await page.getByLabel(`Page ${pageNumber} of`).click()
+  await page.getByLabel(`Page ${pageNumber}`).or(page.getByRole('link', { name: `Page ${pageNumber}` })).click()
 }
 
 export const clickOnPaginationPrevious = async (page: Page) => {
-  await page.getByRole('link', { name: 'Previous page' }).click()
+  await page
+    .getByRole('link', { name: /Previous(\s+page)?/i })
+    .or(page.getByRole('button', { name: /Previous(\s+page)?/i }))
+    .click()
 }
